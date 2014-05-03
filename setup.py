0b7141936036dfefb80cbd3c265d8856e4f255c3
Luiz Irber <luiz.irber@gmail.com>
2014-05-03 19:04:15 -0400

    Add MurmurHash3, SHA-1 to kmer_hash

--- setup.py
+++ setup.py
@@ -56,13 +56,14 @@ EXTRA_OBJS.extend(path_join("third-party", "bzip2", bn + ".o") for bn in [
 BUILD_DEPENDS = list(EXTRA_OBJS)
 BUILD_DEPENDS.extend(path_join("lib", bn + ".hh") for bn in [
     "khmer", "khmer_config", "kmer_hash", "hashtable", "counting",
-    "hashbits", "labelhash", "hllcounter"])
+    "hashbits", "labelhash", "hllcounter", "MurmurHash3", "sha1"])
 
 SOURCES = ["khmer/_khmermodule.cc"]
 SOURCES.extend(path_join("lib", bn + ".cc") for bn in [
     "khmer_config", "thread_id_map", "trace_logger", "perf_metrics",
     "read_parsers", "kmer_hash", "hashtable", "hashbits", "labelhash",
-    "counting", "subset", "read_aligner", "hllcounter"])
+    "counting", "subset", "read_aligner", "hllcounter", "MurmurHash3",
+    "sha1"])
 
 EXTRA_COMPILE_ARGS = ['-O3']
 
